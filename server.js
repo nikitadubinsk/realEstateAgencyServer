a32278382b58beceb026560fe376cb543369de6b
Nikita Dubinsky <nikitadubinsk@gmail.com>
2021-06-03 16:18:13 +0300

    fix

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Headers"
   );
   res.header(
     "Access-Control-Allow-Methods",
