e52ee4cbe2e9af8a9d0e003fc6e401b5d8785040
Nikita Dubinsky <nikitadubinsk@gmail.com>
2021-06-03 16:44:58 +0300

    fix

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors());
 // Настройка CORS
 app
   .use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin: *");
     res.header("Access-Control-Allow-Credentials", true);
     res.header(
       "Access-Control-Allow-Headers",
