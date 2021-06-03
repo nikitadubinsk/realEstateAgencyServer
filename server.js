fe743c3b7a9026d30477ceceb8a03cf733054059
Nikita Dubinsky <nikitadubinsk@gmail.com>
2021-06-03 15:25:33 +0300

    fix

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(
 
 // Настройка CORS
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
