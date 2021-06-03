cc97991e5164e24f0c56a822431b45d9841b3fd0
Nikita Dubinsky <nikitadubinsk@gmail.com>
2021-06-03 15:36:17 +0300

    fix

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ app.use(cors());
 
 // Настройка CORS
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://maison.std-704.ist.mospolytech.ru"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
